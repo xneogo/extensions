32f7cfea79a56f16406a71bc7e3885b0f5019d23
wangxinyu <19482929+Bishoptylaor@users.noreply.github.com>
2025-07-01 18:28:54 +0800

    [feature]<xmail>: add email handling and sending functionality 📧

---
+++ xmail/mail.go
@@ -0,0 +1,117 @@
+/*
+ *  ┏┓      ┏┓
+ *┏━┛┻━━━━━━┛┻┓
+ *┃　　　━　　  ┃
+ *┃   ┳┛ ┗┳   ┃
+ *┃           ┃
+ *┃     ┻     ┃
+ *┗━━━┓     ┏━┛
+ *　　 ┃　　　┃神兽保佑
+ *　　 ┃　　　┃代码无BUG！
+ *　　 ┃　　　┗━━━┓
+ *　　 ┃         ┣┓
+ *　　 ┃         ┏┛
+ *　　 ┗━┓┓┏━━┳┓┏┛
+ *　　   ┃┫┫  ┃┫┫
+ *      ┗┻┛　 ┗┻┛
+ @Time    : 2024/10/28 -- 13:50
+ @Author  : 亓官竹 ❤️ MONEY
+ @Copyright 2024 亓官竹
+ @Description: mail.go
+*/
+
+package xmail
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/mail"
+	"net/smtp"
+	"strings"
+)
+
+type Email struct {
+	host   string
+	from   mail.Address
+	passwd string
+}
+
+func NewEmail(host, from, passwd, fromName string) *Email {
+	e := &Email{
+		host:   host,
+		from:   mail.Address{Name: fromName, Address: from},
+		passwd: passwd,
+	}
+
+	return e
+}
+
+func (m *Email) auth() smtp.Auth {
+	return smtp.PlainAuth(
+		"",
+		m.from.Address,
+		m.passwd,
+		m.host,
+	)
+}
+
+func (m *Email) encodeRFC2047(s string) string {
+	// use mail's rfc2047 to encode any string
+	addr := mail.Address{Name: s}
+	return strings.Trim(addr.String(), " <>")
+}
+
+func (m *Email) rmdup(addrs []string) []string {
+
+	ck := make(map[string]bool)
+	for _, a := range addrs {
+		if len(a) > 0 {
+			ck[a] = true
+		}
+	}
+
+	rv := make([]string, 0)
+
+	for k, _ := range ck {
+		rv = append(rv, k)
+	}
+
+	return rv
+}
+
+func (m *Email) Html(to, cc []string, subject, body string) error {
+	to = m.rmdup(to)
+	cc = m.rmdup(cc)
+
+	header := make(map[string]string)
+	header["From"] = m.from.String()
+	header["To"] = strings.Join(to, ";")
+	header["Cc"] = strings.Join(cc, ";")
+
+	header["Subject"] = m.encodeRFC2047(subject)
+	header["MIME-Version"] = "1.0"
+	// header["Content-Type"] = "text/plain; charset=\"utf-8\""
+	header["Content-Type"] = "text/html; charset=\"utf-8\""
+	header["Content-Transfer-Encoding"] = "base64"
+
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one
+	toList := append(to, cc...)
+	toList = m.rmdup(toList)
+	// fmt.Println(toList)
+	err := smtp.SendMail(
+		m.host+":25",
+		m.auth(),
+		m.from.Address,
+		toList,
+		[]byte(message),
+	)
+
+	return err
+}
