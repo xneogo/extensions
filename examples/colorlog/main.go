392863d8ef3a97637d41c535476b8bfe303b06aa
wangxinyu <19482929+Bishoptylaor@users.noreply.github.com>
2025-07-03 11:36:00 +0800

    [refactor]<colorlog>: remove old test file and add example main.go 🗑️

--- examples/colorlog/main.go
+++ examples/colorlog/main.go
@@ -14,24 +14,10 @@
  *　　 ┗━┓┓┏━━┳┓┏┛
  *　　   ┃┫┫  ┃┫┫
  *      ┗┻┛　 ┗┻┛
- @Time    : 2025/5/14 -- 17:26
+ @Time    : 2025/7/3 -- 11:25
  @Author  : 亓官竹 ❤️ MONEY
  @Copyright 2025 亓官竹
- @Description: xother xother/mm.go
+ @Description: colorlog examples/colorlog/main.go
 */
 
-package xother
-
-func Max[num int64 | int32 | int](a, b num) num {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func Min[num int64 | int32 | int](a, b num) num {
-	if a < b {
-		return a
-	}
-	return b
-}
+package colorlog
